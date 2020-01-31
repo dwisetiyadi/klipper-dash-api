1a59dc1bb87c113efec00c761677034ad396165b
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-01-31 15:28:03 +0700

    refactor & bugfix

--- src/config/Router.ts
+++ src/config/Router.ts
@@ -4,10 +4,8 @@
 
 import RouterHome from '../modules/home/RouterHome';
 import RouterAuth from '../modules/auth/RouterAuth';
-import RouterConsole from '../modules/console/RouterConsole';
 
 export default [
   ...RouterHome,
   ...RouterAuth,
-  ...RouterConsole,
 ];
