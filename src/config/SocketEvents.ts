a1ebef2a0bf32188669de1cffae6e64003d16ce1
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-08 23:35:47 +0700

    refactor

--- src/config/SocketEvents.ts
+++ src/config/SocketEvents.ts
@@ -5,11 +5,11 @@
 import socketIO from 'socket.io';
 import SocketConsoleModule from '../modules/console/SocketConsole';
 
-const sequenceNumberByClient = new Map();
-
+let clients: any;
 
 export default (socket: socketIO.Socket, port: any, parser: any) => {
   console.log(`socketId ${socket.id} connected`);
+  clients[socket.id] = socket;
 
   // socket on modules
   SocketConsoleModule(socket, port, parser);
@@ -21,7 +21,7 @@ export default (socket: socketIO.Socket, port: any, parser: any) => {
     console.log('Socket.IO Error: ', err.stack);
   });
   socket.on('disconnect', () => {
-    sequenceNumberByClient.delete(socket);
+    delete clients[socket.id];
     console.log(`socketId ${socket.id} disconnected`);
   });
 };
