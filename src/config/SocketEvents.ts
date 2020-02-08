b0481d6266e6eddbb08aecdffcd324d138a61ad0
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-09 00:04:24 +0700

    refactor

--- src/config/SocketEvents.ts
+++ src/config/SocketEvents.ts
@@ -5,8 +5,11 @@
 import socketIO from 'socket.io';
 import SocketConsoleModule from '../modules/console/SocketConsole';
 
+const allClients: any[] = [];
+
 export default (socket: socketIO.Socket, port: any, parser: any) => {
   console.log(`socketId ${socket.id} connected`);
+  allClients.push(socket);
 
   // socket on modules
   SocketConsoleModule(socket, port, parser);
@@ -16,5 +19,10 @@ export default (socket: socketIO.Socket, port: any, parser: any) => {
   });
   socket.on('error', (err) => {
     console.log('Socket.IO Error: ', err.stack);
+  });
+  socket.on('disconnect', () => {
+    console.log(`socketId ${socket.id} disconnected`);
+    const i = allClients.indexOf(socket);
+    allClients.splice(i, 1);
   });
 };
