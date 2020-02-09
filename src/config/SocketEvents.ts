de59f34f9294cb5144a6867925976f3c432dbf66
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-09 12:10:39 +0700

    try to fixing multiple connection on client refresh

--- src/config/SocketEvents.ts
+++ src/config/SocketEvents.ts
@@ -3,13 +3,30 @@
  */
 
 import socketIO from 'socket.io';
+import SerialPort from 'serialport';
+import Readline from '@serialport/parser-readline';
+import * as Data from '../../config/Application.json';
+
 import SocketConsoleModule from '../modules/console/SocketConsole';
+import {
+  SocketResponse,
+} from '../utilities';
 
 export default (socket: socketIO.Socket) => {
+  // serial port connection
+  const port = new SerialPort(Data.printer.connection.port, (err: any) => {
+    if (err) {
+      if ([...err.message.matchAll(/Error: No such file or directory, cannot open/gm)].length > 0) {
+        socket.emit('gcodeResponse', SocketResponse(500, err.message));
+      }
+    }
+  });
+  const parser = new Readline();
+
   console.log(`socketId ${socket.id} connected`);
 
   // socket on modules
-  SocketConsoleModule(socket);
+  SocketConsoleModule(socket, port, parser);
 
   socket.on('end', (client) => {
     socket.disconnect(client);
--- src/modules/console/SocketConsole.ts
+++ src/modules/console/SocketConsole.ts
@@ -3,32 +3,12 @@
  */
 
 import socketIO from 'socket.io';
-import SerialPort from 'serialport';
-import Readline from '@serialport/parser-readline';
-import * as Data from '../../config/Application.json';
 
 import {
   SocketResponse,
 } from '../../utilities';
 
-export default (socket: socketIO.Socket) => {
-  // serial port connection
-  const port = new SerialPort(Data.printer.connection.port, (err: any) => {
-    if (err) {
-      if ([...err.message.matchAll(/Error: No such file or directory, cannot open/gm)].length > 0) {
-        socket.emit('gcodeResponse', SocketResponse(500, err.message));
-      }
-    }
-  });
-  const parser = new Readline();
-
-  // some helpers
-  // const onPortConnectionError = (err: any) => {
-  //   if ([...err.message.matchAll(/Error: No such file or directory, cannot open/gm)].length > 0) return true;
-  //   return false;
-  // };
-
-  // socket and serial run
+export default (socket: socketIO.Socket, port: any, parser: any) => {
   port.pipe(parser);
   parser.on('data', (line: any) => {
     socket.emit('gcodeResponse', SocketResponse(200, line));
