3325975e6651809af5e6c122e048cd89a943a3c5
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-09 10:02:52 +0700

    refactor

--- src/config/SocketEvents.ts
+++ src/config/SocketEvents.ts
@@ -8,7 +8,8 @@ import SocketConsoleModule from '../modules/console/SocketConsole';
 export default (socket: socketIO.Socket, port: any, parser: any) => {
   console.log(`socketId ${socket.id} connected`);
   console.log('rooms: ', socket.rooms);
-  console.log('listeners: ', socket.listeners);
+  console.log('listeners Gcode: ', socket.listeners('gcode'));
+  console.log('listeners Gcode Response: ', socket.listeners('gcodeResponse'));
 
   // socket on modules
   SocketConsoleModule(socket, port, parser);
