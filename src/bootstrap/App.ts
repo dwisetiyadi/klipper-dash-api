64dc6c4c6fe60f502a251f04f8139f6f05b18311
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-09 15:28:20 +0700

    try to fixing multiple connection on client refresh

--- src/bootstrap/App.ts
+++ src/bootstrap/App.ts
@@ -48,7 +48,6 @@ const App = async (): Promise<void> => {
     io.on('connection', (socket: socketIO.Socket) => {
       port.pipe(parser);
       parser.on('data', (line: any) => {
-        console.log(line);
         socket.emit('gcodeResponse', SocketResponse(200, line));
       });
       SocketEvents(socket, port, parser);
