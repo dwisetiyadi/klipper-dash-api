9ec09cf89cb267f621c43caebca674702277aed2
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-02-09 09:05:50 +0700

    refactor

--- src/bootstrap/App.ts
+++ src/bootstrap/App.ts
@@ -46,11 +46,12 @@ const App = async (): Promise<void> => {
   
   const namespace = (process.env.SOCKET_PATH) ? `/${process.env.SOCKET_PATH}` : '';
   const io = socketIO(server.listener, {
-    pingInterval: 60000,
-    pingTimeout: 120000,
-  });
+    // pingInterval: 60000,
+    // pingTimeout: 120000,
+    transports: ['websocket'],
+  }).of(namespace);
   io.use((socket: socketIO.Socket, next) => { SocketMidlewares(socket, next); });
-  io.on('connection', (socket: socketIO.Socket) => { SocketEvents(socket, Port, Parser); });
+  io.once('connection', (socket: socketIO.Socket) => { SocketEvents(socket, Port, Parser); });
 
   server.route(Router);
 
