2735e0e0aca8e01d2401f9e461b3cee24e1c4717
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-05-02 16:18:10 +0700

    register module to main router

--- src/config/Router.ts
+++ src/config/Router.ts
@@ -4,8 +4,10 @@
 
 import RouterHome from '../modules/home/RouterHome';
 import RouterAuth from '../modules/auth/RouterAuth';
+import RouterFiles from '../modules/files/RouterFiles';
 
 export default [
   ...RouterHome,
   ...RouterAuth,
+  ...RouterFiles,
 ];
