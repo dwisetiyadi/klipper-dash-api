6ea5bc3bdb3ae2e4e227bcd10bf8b3004333c212
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-08-03 09:17:41 +0700

    create "delete" function, and refactor another function.

--- src/modules/files/RouterFiles.ts
+++ src/modules/files/RouterFiles.ts
@@ -21,16 +21,16 @@ const Routes = [
     path: '/file/{gcode}',
     handler: {
       file: {
-        path: (req: Hapi.Request): string => Files.read(req),
+        path: (req: Hapi.Request, res: Hapi.ResponseToolkit): string => Files.read(req, res),
         confine: false,
       },
     },
   },
-  // {
-  //   method: 'DELETE',
-  //   path: '/file/{gcode}',
-  //   handler: (req: Hapi.Request, res: Hapi.ResponseToolkit): object => Files.read(req, res),
-  // },
+  {
+    method: 'DELETE',
+    path: '/file/{gcode}',
+    handler: (req: Hapi.Request, res: Hapi.ResponseToolkit): object => Files.delete(req, res),
+  },
 ];
 
 export default Routes;
--- src/modules/files/controllers/Files.ts
+++ src/modules/files/controllers/Files.ts
@@ -14,29 +14,52 @@ const pathUpload = `${path.resolve(os.homedir())}/uploads`;
 
 class Files {
   static listing = (req: Hapi.Request, res: Hapi.ResponseToolkit) => {
-    if (!fs.existsSync(pathUpload)) {
-      fs.mkdirpSync(pathUpload);
+    try {
+      if (!fs.existsSync(pathUpload)) {
+        fs.mkdirpSync(pathUpload);
+      }
+  
+      const files = fs.readdirSync(pathUpload)
+        .map((file) => {
+          const stat = fs.statSync(`${pathUpload}/${file}`);
+          const ext = path.extname(`${pathUpload}/${file}`);
+          return {
+            fullname: file,
+            filename: file.replace(ext, ''),
+            ext: ext.replace('.', ''),
+            size: stat.size,
+            created: stat.birthtime,
+          };
+        })
+        .filter((file) => file.ext === 'gcode');
+        
+      return HttpResponse(200, files, res);
+    } catch (error) {
+      console.log(error);
+      if (error.message) return HttpResponse(400, error.message, res);
+      return HttpResponse(500, error, res);
     }
-
-    const files = fs.readdirSync(pathUpload)
-      .map((file) => {
-        const stat = fs.statSync(`${pathUpload}/${file}`);
-        const ext = path.extname(`${pathUpload}/${file}`);
-        return {
-          fullname: file,
-          filename: file.replace(ext, ''),
-          ext: ext.replace('.', ''),
-          size: stat.size,
-          created: stat.birthtime,
-        };
-      })
-      .filter((file) => file.ext === 'gcode');
-      
-    return HttpResponse(200, files, res);
   };
 
-  static read = (req: Hapi.Request) => {
-    return `${pathUpload}/${req.params.gcode}`;
+  static read = (req: Hapi.Request, res: Hapi.ResponseToolkit) => {
+    try {
+      return `${pathUpload}/${req.params.gcode}`;
+    } catch (error) {
+      console.log(error);
+      if (error.message) return HttpResponse(400, error.message, res);
+      return HttpResponse(500, error, res);
+    }
+  };
+
+  static delete = (req: Hapi.Request, res: Hapi.ResponseToolkit) => {
+    try {
+      const rv = fs.removeSync(`${pathUpload}/${req.params.gcode}`);
+      return HttpResponse(200, rv, res);
+    } catch (error) {
+      console.log(error);
+      if (error.message) return HttpResponse(400, error.message, res);
+      return HttpResponse(500, error, res);
+    }
   };
 };
 
