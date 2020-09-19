5f4d20530c5fe77fabc97b40f228a9d817e97f72
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-09-19 13:15:40 +0700

    test error

--- src/config/AuthSocket.ts
+++ src/config/AuthSocket.ts
@@ -7,15 +7,16 @@ import * as Data from './Application.json';
 import { decodeToken } from '../utilities';
 
 export default (socket: socketIO.Socket, next: any) => {
-  try {
-    if (!socket.handshake.query.token) throw 'missing parameter token.';
-    const decoded: any = decodeToken(socket.handshake.query.token);
-    if (!decoded) throw 'invalid token.';
-    const person = Data.users.find((obj: any) => obj.username === decoded.agent.userName && obj.password === decoded.agent.password);
-    if (!person) return next();
-    throw 'authentication error';
-  } catch(error) {
-    console.log(error);
-    return next(new Error(error));
-  }
+  // try {
+  //   if (!socket.handshake.query.token) throw 'missing parameter token.';
+  //   const decoded: any = decodeToken(socket.handshake.query.token);
+  //   if (!decoded) throw 'invalid token.';
+  //   const person = Data.users.find((obj: any) => obj.username === decoded.agent.userName && obj.password === decoded.agent.password);
+  //   if (!person) return next();
+  //   throw 'authentication error';
+  // } catch(error) {
+  //   console.log(error);
+  //   return next(new Error(error));
+  // }
+  return next(new Error('test error'));
 };
