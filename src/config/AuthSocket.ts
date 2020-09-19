c5fed06e7fbdac735a3955ae923201ec7a8e2739
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2020-09-19 17:21:33 +0700

    refactor

--- src/config/AuthSocket.ts
+++ src/config/AuthSocket.ts
@@ -4,20 +4,18 @@
 
 import socketIO from 'socket.io';
 import * as Data from './Application.json';
-import { decodeToken, SocketResponse } from '../utilities';
+import { decodeToken } from '../utilities';
 
 export default (socket: socketIO.Socket, next: any) => {
-  // try {
-  //   if (!socket.handshake.query.token) throw 'missing parameter token.';
-  //   const decoded: any = decodeToken(socket.handshake.query.token);
-  //   if (!decoded) throw 'invalid token.';
-  //   const person = Data.users.find((obj: any) => obj.username === decoded.agent.userName && obj.password === decoded.agent.password);
-  //   if (!person) return next();
-  //   throw 'authentication error';
-  // } catch(error) {
-  //   console.log(error);
-  //   return next(new Error(error));
-  // }
-  socket.emit('gcodeResponse', SocketResponse(500, 'test error'));
-  // return next(new Error('test error'));
+  try {
+    if (!socket.handshake.query.token) throw 'missing parameter token.';
+    const decoded: any = decodeToken(socket.handshake.query.token);
+    if (!decoded) throw 'invalid token.';
+    const person = Data.users.find((obj: any) => obj.username === decoded.agent.userName && obj.password === decoded.agent.password);
+    if (!person) return next();
+    throw 'authentication error';
+  } catch(error) {
+    console.log(error);
+    return next(new Error(error));
+  }
 };
